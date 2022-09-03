64f28b921f0541b8c8ff6e02fb23815da3329fe2
ajstephens1995 <ajstephens1995@gmail.com>
2022-09-03 15:45:43 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = ""
+    for (let i = -1; i >= (string.length * -1); i--) {
 
+        reversedString += string.substr(i, 1);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
