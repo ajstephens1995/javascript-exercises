f9c3bbabd545410c58a5a87d939c2bc0a94d5521
ajstephens1995 <ajstephens1995@gmail.com>
2022-09-03 14:30:12 -0400

    Complete js exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let outputString = "";
+    let i = 0
+    
+    if (num < 0) {
+        return "ERROR";
+    }
+    while (i < num) {
+        outputString += string;
+        i++
+    }
+    return outputString;
 };
 
 // Do not edit below this line
