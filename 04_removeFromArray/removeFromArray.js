ca51ad8f5f282d9fff1873bd30685b4fb30908df
ajstephens1995 <ajstephens1995@gmail.com>
2022-09-03 23:41:53 -0400

    Complete 04 and 05 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    newArray = arguments[0]
+    for (i = 0; i < arguments[0].length; i++) {
+        for (ii = 1; ii < arguments.length; ii++) {
+           if (newArray[i] === arguments[ii]) {
+            newArray.splice(i, 1);
+            i--
+           }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+    sum = 0;
+    if (typeof intOne !== "number"|| typeof intTwo !== "number") {
+        return "ERROR"
+    }
+    if (intOne < 0 || intTwo < 0) {
+        return "ERROR"
+    }
+    if (intOne < intTwo) {
+        for (i = intOne; i <= intTwo; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    if (intTwo < intOne) {
+        for (i = intTwo; i <= intOne; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
