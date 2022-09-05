703af4aca69dc022c59835ed961ea0f7b0a9a8d0
ajstephens1995 <ajstephens1995@gmail.com>
2022-09-05 12:32:28 -0400

    Complete exercises 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fval) {
+  let cval = Math.round((((fval - 32) * (5/9)) * 10)) / 10;
+  return cval;
 };
 
-const ctof = function() {
-
+const ctof = function(cval) {
+  let fval = Math.round((((cval * (9/5)) + 32) * 10)) / 10;
+  return fval;
 };
 
 // Do not edit below this line
