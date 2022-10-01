72722161b43f6794641cd14b994b09fa120024dd
ajstephens1995 <99112070+ajstephens1995@users.noreply.github.com>
2022-10-01 17:27:03 +0000

    complete calculator, Fibonacci, and palindrome exercies

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  } return total;
+    
+  };
+
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	let total = 1;
+  for (let i = 0; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function(string) {
 
+  const array = (Array.from(string.toLowerCase())).filter((item) => {
+    if (item === " ") { return false }
+    else if (item === "!") { return false }
+    else if (item === ".") { return false }
+    else if (item === ",") { return false }
+    else if (item === "?") { return false }
+    else return true;
+  });
+  const reverseArray = [...array].reverse();
+  return array.join("") === reverseArray.join("") ? true : false
 };
-
 // Do not edit below this line
 module.exports = palindromes;
